2d5aebaae493a3f5f30022b17f3f4dfec5650f87
Jon Goldberg <jon@megaphonetech.com>
2020-02-03 17:58:08 -0500

    closes #2 - update financial item when moving a contribution

--- CRM/LCD/MoveContrib/BAO/MoveContrib.php
+++ CRM/LCD/MoveContrib/BAO/MoveContrib.php
@@ -9,6 +9,15 @@ class CRM_LCD_MoveContrib_BAO_MoveContrib {
     $id = array('contribution' => $params['contribution_id']);
     $contribution = CRM_Contribute_BAO_Contribution::create($params, $id);
 
+    // Update the financial items' contact IDs as well.
+    $fiSql = "UPDATE civicrm_financial_item cfi
+    JOIN civicrm_entity_financial_trxn cefti ON cefti.entity_id = cfi.id
+    JOIN civicrm_financial_trxn cft ON cefti.entity_table = 'civicrm_financial_item' AND cft.id = cefti.financial_trxn_id
+    JOIN civicrm_entity_financial_trxn ceftc ON ceftc.financial_trxn_id = cft.id
+    SET cfi.contact_id = {$params['change_contact_id']}
+    WHERE ceftc.entity_table = 'civicrm_contribution' AND ceftc.entity_id = {$params['contribution_id']}";
+    CRM_Core_DAO::executeQuery($fiSql);
+
     // record activity for moving contribution
     if ($contribution) {
       $subject = "Contribution #{$params['contribution_id']} Moved";
