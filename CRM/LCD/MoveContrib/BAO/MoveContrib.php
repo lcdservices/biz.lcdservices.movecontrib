13654af8587f6facdec79acec6eb34d3bff09134
Brian Shaughnessy <brian@lcdservices.biz>
2020-12-22 20:47:57 -0500

    fixes #2 update financial item when moving a contrib

--- CRM/LCD/MoveContrib/BAO/MoveContrib.php
+++ CRM/LCD/MoveContrib/BAO/MoveContrib.php
@@ -6,9 +6,27 @@
 class CRM_LCD_MoveContrib_BAO_MoveContrib {
 
   static function moveContribution($params) {
-    $id = array('contribution' => $params['contribution_id']);
     $params['id'] = $params['contribution_id'];
     $contribution = CRM_Contribute_BAO_Contribution::create($params);
+
+    // Update the financial items' contact IDs as well.
+    $sql = "
+      UPDATE civicrm_financial_item cfi
+      JOIN civicrm_entity_financial_trxn cefti
+        ON cefti.entity_id = cfi.id
+      JOIN civicrm_financial_trxn cft
+        ON cefti.entity_table = 'civicrm_financial_item'
+        AND cft.id = cefti.financial_trxn_id
+      JOIN civicrm_entity_financial_trxn ceftc
+        ON ceftc.financial_trxn_id = cft.id
+      SET cfi.contact_id = %1
+      WHERE ceftc.entity_table = 'civicrm_contribution'
+        AND ceftc.entity_id = %1
+    ";
+    CRM_Core_DAO::executeQuery($sql, [
+      1 => [$params['change_contact_id'], 'Positive'],
+      2 => [$params['contribution_id'], 'Positive'],
+    ]);
 
     // record activity for moving contribution
     if ($contribution) {
@@ -20,14 +38,14 @@ class CRM_LCD_MoveContrib_BAO_MoveContrib {
         'name'
       );
 
-      $activityParams = array(
+      $activityParams = [
         'source_contact_id' => $params['current_contact_id'],
         'activity_type_id' => $activityTypeID,
         'activity_date_time' => date('YmdHis'),
         'subject' => $subject,
         'details' => $details,
         'status_id' => 2,
-      );
+      ];
 
       $session = CRM_Core_Session::singleton();
       $id = $session->get('userID');
