d77081f56acb653295fe6e5789616ad2fd627bcc
Artur Śmigielski <kontakt@iprosoft.pl>
2025-06-13 13:26:49 +0200

    Fix problem with move contribution from deleted contact

--- movecontrib.php
+++ movecontrib.php
@@ -31,15 +31,13 @@ function movecontrib_civicrm_enable() {
 }
 
 function movecontrib_civicrm_searchTasks($objectType, &$tasks) {
-
   if ($objectType === 'contribution' && CRM_Core_Permission::check('allow Move Contribution')) {
     $tasks[] = [
       'title' => E::ts('Move Contribution'),
       'class' => 'CRM_LCD_MoveContrib_Form_Task',
       // Code suggests result key is likely meaningless.
       'result' => TRUE,
-      'is_single_mode' => TRUE,
-      'title_single_mode' => E::ts('Move Contribution'),
+      'is_single_mode' => FALSE,
       'name' => E::ts('Move Contribution'),
       'url' => 'civicrm/contribute/task?reset=1&task_item=move',
       'key' => 'move',
@@ -47,7 +45,18 @@ function movecontrib_civicrm_searchTasks($objectType, &$tasks) {
     ];
   }
 }
-
+function movecontrib_civicrm_links(string $op, ?string $objectName, $objectID, array &$links, ?int &$mask, array &$values): void {
+  if ($objectName !== 'Contribution' || $op !== 'contribution.selector.row' || !CRM_Core_Permission::check('allow Move Contribution')) {
+    return;
+  }
+  $links[] = [
+    'name' => E::ts('Move Contribution'),
+    'url' => 'civicrm/movecontrib',
+    "qs" => "reset=1&task_item=move&id=%%id%%&cid=%%cid%%&context=%%cxt%%",
+    'title' => E::ts('Move Contribution'),
+    'weight' => 130,
+  ];
+}
 /**
  *Implementation of hook_civicrm_permission
  * @param array $permissions
